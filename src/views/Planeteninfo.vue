d847b06cc2a45804d960eff7719655243bb739ef
Seraphine Thuerler <seraphine.thuerler@stud.hslu.ch>
2020-11-19 13:57:42 +0100

    Planeteninfo Trying to Style
    Doesn't work for some reason

--- src/views/Planeteninfo.vue
+++ src/views/Planeteninfo.vue
@@ -1,36 +1,41 @@
 <template>
-  <div class="main">
-    <h1>{{ msg }}</h1>
-    <img class="planetAsset">
-    <h1>{{ $route.params.planetname }}</h1>
-    <img class="logo">
-    <div class="planetdescription">hier kommt text</div>
-  </div>
+
+  <div class="bg-wrapper">
+    <div class="bg">
+      <!-- <h1>{{ msg }}</h1>
+      <img class="planetAsset">
+      <h1>{{ $route.params.planetname }}</h1>
+      <img class="logo">
+      <div class="planetdescription">hier kommt text</div>
+ -->
+      <img class="starsbackground" src="../assets/stars2.svg">
+      <div class="btns">
+          <a href=""><img class="sammlungBtn" src="../assets/sammlungBtn.svg"> <router-link to="/sam"></router-link></a>
+          <a href=""><img class="mapBtn" src="../assets/mapBtn.svg"> <router-link to="/loc"></router-link></a>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-export default {
+/* export default {
   name: 'planetenView',
   props: {
     msg: String
   }
+} */
+export default {
+  name: 'planetenView',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .starsbackground {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width:75%;
+  }
+
 </style>
